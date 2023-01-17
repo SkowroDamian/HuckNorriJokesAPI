9abb0367b84e93f760626eb45cae7bc50d9f5dc1
damian <skowronskid@icloud.com>
2023-01-17 18:54:38 +0100

    first commit

---
+++ HuckNorrisJokesAPI/HuckNorrisJokesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuckNorrisJokesAPIApp.swift
+//  HuckNorrisJokesAPI
+//
+//  Created by Damian Skowroński on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HuckNorrisJokesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
